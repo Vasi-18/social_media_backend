c000ab28adc389dd370bab759b7f5a7096d0c9b6
Vasi-18 <vasanthkumar1822000@gmail.com>
2023-09-16 18:35:47 +0530

    last deploy

--- index.js
+++ index.js
@@ -5,21 +5,28 @@ import dotenv from "dotenv";
 import cors from "cors";
 // Routes
 import AuthRoute from "./Routes/AuthRoute.js";
-import UserRoute from "./Routes/UserRoute.js"
-import PostRoute from "./Routes/PostRoute.js"
-import UploadRoute from "./Routes/UploadRoute.js"
+import UserRoute from "./Routes/UserRoute.js";
+import PostRoute from "./Routes/PostRoute.js";
+import UploadRoute from "./Routes/UploadRoute.js";
 
 const app = express();
 
 // to serve images inside public folder
-app.use(express.static('public')); 
-app.use('/images', express.static('images'));
+app.use(express.static("public"));
+app.use("/images", express.static("images"));
 
+const corsOpts = {
+  origin: "*",
+
+  methods: ["GET", "POST"],
+
+  allowedHeaders: ["Content-Type"],
+};
 
 // Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors(corsOpts));
 dotenv.config();
 
 mongoose
@@ -36,7 +43,7 @@ mongoose
 
 //usage of Routes
 
-app.use('/auth', AuthRoute);
-app.use('/user', UserRoute);
-app.use('/post', PostRoute);
-app.use('/upload', UploadRoute);
+app.use("/auth", AuthRoute);
+app.use("/user", UserRoute);
+app.use("/post", PostRoute);
+app.use("/upload", UploadRoute);
